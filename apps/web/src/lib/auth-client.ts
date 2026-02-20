b44b638658514ac21915099f9309dca59676059e
iRazz <hi@irazz.lol>
2026-02-20 18:17:49 +0200

    stuff

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,8 +1,7 @@
-import { env } from "@glare/env/web";
 import { createAuthClient } from "better-auth/react";
 import { adminClient, lastLoginMethodClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_SERVER_URL,
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   plugins: [adminClient(), lastLoginMethodClient()],
 });
